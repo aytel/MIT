79d489dc1092d167a8d29d2470674fca10618283
Arseny Babushkin <arseny.babushkin@gmail.com>
2017-09-12 20:01:00 +0100

    paradigms hw2

---
+++ grade_1/[paradigms]/hw2/hw.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from hashlib import sha1 as hasher
+
+SZ = 2 ** 16
+
+
+def hsh(filename):
+    with open(filename, mode='rb') as filename:
+        h = hasher()
+        up = filename.read(SZ)
+        while up:
+            h.update(up)
+            up = filename.read(SZ)
+    filename.close()
+    return h.hexdigest()
+
+if __name__ == '__main__':
+    diff = dict()
+    go = os.walk(sys.argv[1])
+    for direct, _, files in go:
+        for file in files:
+            path = os.path.join(direct, file)
+            if file[0] == '~' or file[0] == '.' or os.path.islink(path):
+                continue
+            cur = hsh(direct + '\\' + file)
+            if cur not in diff:
+                diff[cur] = []
+            diff[cur].append((direct, file))
+    for _, cl in diff.items():
+        if len(cl) < 2:
+            continue
+        for direct, file in cl:
+            print(file, ':', direct, sep='', end=' / ')
+        print()
