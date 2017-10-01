7feca4d4bc909e5bf705c22ad7a180fa9d356cc5
Arseny Babushkin <arseny.babushkin@gmail.com>
2017-10-01 23:36:28 +0100

    cpp hw3 and algo hw4

--- grade_1/[paradigms]/common.py
+++ grade_1/[paradigms]/common.py
@@ -1,12 +1,25 @@
-def prod(a, b):
-    n = len(a)
-    c = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            for k in range(n):
-                c[i][j] += a[i][k] * b[k][j]
-    return c
-    
-import numpy as np
+class node:
+    def __init__(self, l=None, r=None, x=None):
+        self.key = x
+        self.left = l
+        self.right = r
 
-a = np.array([1, 2, 8], float)
+def insert(root_node, x):
+    if root_node.key == None:
+        node.key = x
+        return
+    if (root_node.key >= x):
+        root_node.left = insert(root_node.left, x)
+    else:
+        root_node.right = insert(root_node.right, x)
+    return
+    raise NotImplementedError
+
+def inorder_traversal(node):
+    if node == None:
+        return
+    inorder_traversal(node.left)
+    print(node.key)
+    inorder_traversal(node.right)
+    return
+    raise NotImplementedError
